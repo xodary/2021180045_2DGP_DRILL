70cb03960e6310ad07b5e5826c421006ba6e9f29
xodary <96102282+xodary@users.noreply.github.com>
2022-09-21 17:42:42 +0900

    dirll 06 완성
    2021180045 황유림

--- 06/rect.py
+++ 06/rect.py
@@ -8,6 +8,7 @@ character = load_image('character.png')
 x = 0
 y = 90
 while(1):
+    
     while (x < 800):
         clear_canvas_now()
         grass.draw_now(400,30)
@@ -32,6 +33,18 @@ while(1):
         character.draw_now(x,y)
         y = y - 2
         delay(0.001)
-
-
-close_canvas()
+    while (x < 400):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        x = x + 2
+        delay(0.001)
+    x = 0
+    
+    while (x < 360):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(400 + 250 * math.sin(math.pi / 180 * x),350 - 250 * math.cos(math.pi / 180 * x))
+        x = x + 10 * math.pi / 180
+        delay(0.001)
+    x = 400close_canvas()
